e273755f426ac8aded0dbb4d3129c4177a0b98da
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-18 11:42:21 -0300

    add styles

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import iconStar from '../../assets/icons/icon-star.svg';
 import ButtonList from './components/ButtonList/ButtonList';
+import styles from './Rating.module.css'
+import iconStar from '../../assets/icons/icon-star.svg';
 
 export default function Rating() {
   return (
-    <section>
-      <img src={iconStar} alt="Star" />
+    <section className={styles.ratingContainer}>
+      <img src={iconStar} alt="Star" className={styles.star}/>
       <h2>How did we do?</h2>
       <p>
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
       <ButtonList />
-      <button>Submit</button>
+      <button className={styles.buttonSubmit}>Submit</button>
     </section>
   );
 }
