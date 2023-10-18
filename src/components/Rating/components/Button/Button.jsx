ee398c81dcd5ecf8ef1a0f2297b8b063d275924d
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-18 11:55:53 -0300

    fix error on select multiple buttons

--- src/components/Rating/components/Button/Button.jsx
+++ src/components/Rating/components/Button/Button.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ number, handleClickRating }) {
+export default function Button({ number, handleClickRating, isAnySelected }) {
   const [isSelected, setIsSelected] = useState(false);
 
   const handleClick = () => {
-    handleClickRating(number)
-    setIsSelected(!isSelected);
+    if (!isAnySelected || isSelected) {
+      handleClickRating(number);
+      setIsSelected(!isSelected);
+    }
   };
 
   return (
     <button
       className={`${styles.button} ${isSelected ? styles.buttonSelected : ''}`}
       onClick={handleClick}
+      disabled={isAnySelected && !isSelected}
     >
       {number}
     </button>
--- src/components/Rating/components/ButtonList/ButtonList.jsx
+++ src/components/Rating/components/ButtonList/ButtonList.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
 import styles from './ButtonList.module.css';
 
 export default function ButtonList({ handleClickRating }) {
+  const [isAnySelected, setIsAnySelected] = useState(false);
+
+  const handleButtonClick = (number) => {
+    handleClickRating(number);
+    setIsAnySelected(!isAnySelected);
+  };
+
   const numbers = [1, 2, 3, 4, 5];
   return (
     <ul className={styles.buttonsList}>
       {numbers.map((number) => (
-        <Button key={number} number={number} handleClickRating={handleClickRating} />
+        <Button
+          key={number}
+          number={number}
+          handleClickRating={handleButtonClick}
+          isAnySelected={isAnySelected}
+        />
       ))}
     </ul>
   );
